37d43a9d593d1bd145175eeaf3b80c0269f6b1cd
denishoornaert <dhoornae@ulb.ac.be>
2018-04-04 11:07:31 +0200

    Update cc12_arch.vhd

--- lab1/cc12_arch.vhd
+++ lab1/cc12_arch.vhd
@@ -2,7 +2,7 @@
 -- VHDL Architecture elech505_lab1_lib.cc12.arch
 --
 -- Created:
---          by - Labo.UNKNOWN (PC-000)
+--          by - Denis Hoornaert
 --          at - 10:46:16 29/03/2018
 --
 -- using Mentor Graphics HDL Designer(TM) 2013.1b (Build 2)
